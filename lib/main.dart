23e0d4700c3542b0f9a9de7b993d658d7ffaa73d
Sudhamsu <gurijalasudhamsu2000@gmail.com>
2022-06-18 21:27:54 +0530

    Code refactoring, adds listview and cards
    Refactors code to enhance interpretation, adds skeleton of listview
    and cards.

--- lib/main.dart
+++ lib/main.dart
@@ -17,35 +17,64 @@ class MyApp extends StatelessWidget {
             child: Text('Penote'),
           ),
         ),
-        bottomNavigationBar: BottomNavigationBar(
-          items: const <BottomNavigationBarItem>[
-            BottomNavigationBarItem(
-              icon: Icon(Icons.add_box_rounded),
-              label: 'To Do',
-            ),
-            BottomNavigationBarItem(
-              icon: Icon(Icons.alarm_sharp),
-              label: 'In Progress',
-            ),
-            BottomNavigationBarItem(
-              icon: Icon(Icons.check_box_rounded),
-              label: 'Done',
-            ),
-          ],
-          currentIndex: 0,
-          backgroundColor: Colors.blue,
-          unselectedItemColor: Colors.white,
-          selectedItemColor: Colors.black,
-        ),
-        body: const Center(
-          child: Text('Hello World'),
-        ),
+        bottomNavigationBar: const BottomMenu(),
+        body: const ItemList(),
         floatingActionButton: const FloatingActionButton(
           onPressed: null,
           child: Icon(Icons.add),
           tooltip: 'Add',
         ),
       ),
+    );
+  }
+}
+
+class BottomMenu extends StatelessWidget {
+  const BottomMenu({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return BottomNavigationBar(
+      items: const <BottomNavigationBarItem>[
+        BottomNavigationBarItem(
+          icon: Icon(Icons.add_box_rounded),
+          label: 'To Do',
+        ),
+        BottomNavigationBarItem(
+          icon: Icon(Icons.alarm_sharp),
+          label: 'In Progress',
+        ),
+        BottomNavigationBarItem(
+          icon: Icon(Icons.check_box_rounded),
+          label: 'Done',
+        ),
+      ],
+      currentIndex: 0,
+      backgroundColor: Colors.grey[200],
+      unselectedItemColor: Colors.grey[800],
+      selectedItemColor: Colors.blue,
+    );
+  }
+}
+
+class ItemList extends StatelessWidget {
+  const ItemList({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return ListView(
+      children: [
+        for (int i = 0; i < 20; ++i)
+          const Card(
+            child: SizedBox(
+              width: double.maxFinite,
+              height: 100,
+              child: Center(
+                child: Text('data'),
+              ),
+            ),
+          ),
+      ],
     );
   }
 }
